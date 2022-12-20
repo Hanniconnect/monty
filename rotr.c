dbbd284ff976b9fa0b17b216e42e90a77f5aa50a
Hanniconnect <hanniplexdacomplex@gmail.com>
2022-12-19 17:24:21 -0800

    rotr.c

--- rotr.c
+++ rotr.c
@@ -5,22 +5,22 @@
  *@counter: line_number
  *Return: no return
 */
-void f_rotl(stack_t **head,  __attribute__((unused)) unsigned int counter)
+void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter)
 {
-	stack_t *tmp = *head, *aux;
+	stack_t *copy;
 
+	copy = *head;
 	if (*head == NULL || (*head)->next == NULL)
 	{
 		return;
 	}
-	aux = (*head)->next;
-	aux->prev = NULL;
-	while (tmp->next != NULL)
+	while (copy->next)
 	{
-		tmp = tmp->next;
+		copy = copy->next;
 	}
-	tmp->next = *head;
-	(*head)->next = NULL;
-	(*head)->prev = tmp;
-	(*head) = aux;
+	copy->next = *head;
+	copy->prev->next = NULL;
+	copy->prev = NULL;
+	(*head)->prev = copy;
+	(*head) = copy;
 }
